dcf60ddfdd7e03210fb0299e7513eda7209c1044
elmira <ingefaerel@localhost.localdomain>
2025-03-14 19:55:28 +0100

    fixed for std pc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Use 'react-dom/client' for React 18
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-// Create a root element for React 18
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
