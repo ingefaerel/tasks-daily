233f8625ac49cb371dd146d0bcae317aedf1ae24
elmira <ingefaerel@localhost.localdomain>
2025-03-14 15:31:18 +0100

    changed host

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Your backend URL
+  baseURL: "http://10.147.19.42:5000", // Your backend URL
 });
 
 export default api;
